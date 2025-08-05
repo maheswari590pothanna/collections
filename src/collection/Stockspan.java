062479386d97c0487314dcc1b81888c6ab525d5a
maheswari590pothanna <maheswari98901@gmail.com>
2025-08-05 17:16:24 +0530

    commit

--- src/collection/Stockspan.java
+++ src/collection/Stockspan.java
@@ -3,5 +3,18 @@ package collection;
 import java.util.Stack;
 
 public class Stockspan {
+    private static void calculatespan(int[] ar) {
+        for(int i=0;i<ar.length;i++){
+            int span=1;
+            for(int j=i-1;j>=0 && ar[j]<ar[i];j--){
+                span++;
+            }
+            System.out.println(span);
+        }
+    }
+    public static void main(String[] args) {
+        int[] ar={23,34,1,7,45,8};
+        calculatespan(ar);
+    }
 }
 
